5d0ef195b268e2b66d0b36797d5c73db4246fb9e
Nipun2001M <nipun20010328@gmail.com>
2025-02-20 09:41:50 +0530

    feat:protected routes added

--- routes/router.go
+++ routes/router.go
@@ -11,9 +11,9 @@ import (
 
 func GetRoutes() *mux.Router{
 	router:=mux.NewRouter()
-	router.HandleFunc("/orders",handlers.CreateOrder).Methods(http.MethodPost)
+	router.Handle("/orders",middleware.AuthMiddleware(http.HandlerFunc(handlers.CreateOrder))).Methods(http.MethodPost)
 	router.Handle("/orders/{id}",middleware.AuthMiddleware(http.HandlerFunc(handlers.GetOrderById))).Methods(http.MethodGet)
-	router.HandleFunc("/orders/payments",handlers.MakePayment).Methods(http.MethodPost)
+	router.Handle("/orders/payments",middleware.AuthMiddleware(http.HandlerFunc(handlers.MakePayment))).Methods(http.MethodPost)
 	router.HandleFunc("/register",auth.RegisterUser).Methods(http.MethodPost)
 	router.HandleFunc("/login",auth.Login).Methods(http.MethodPost)
 	return router;
